b9fec6ac7e7e7fdfd889e71ea7900cadb7517aec
Gabriela Lima <gabrielalima558@gmail.com>
2023-07-23 20:36:44 -0300

    lista pensamens vindo do json server mock

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -11,9 +12,16 @@ export class ListarPensamentoComponent implements OnInit {
   //essa lista virá do backend
   listaPensamentos: Pensamento[] = [];
 
-  constructor() { }
+  //injetar o service de pensamento
+  constructor(private service: PensamentoService) { }
 
+  //ngOnInit faz parte do ciclo de vida do componente, nesse caso é chamado assim que o componente é iniciado
   ngOnInit(): void {
+    //a lista daqui(this.listaPensamentos) vai receber a lista que vem do observable através do subscribe que notifica
+    //sempre que recebe um valor
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
   }
 
 }
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Pensamento } from './pensamento';
+import { Observable } from 'rxjs';
 
 @Injectable({
   //com esse root quer dizer que esse serviço poderá ser injetado em qualquer parte da aplicação
   providedIn: 'root'
 })
 export class PensamentoService {
-
+  private readonly API = 'http://localhost:3000/pensamentos'
   //injeção de dependência no Angular é feita através do construtor
   constructor(private http: HttpClient) { }
+
+  //importar o HttpClientModule no app.module.ts pra dar certo isso dentro do listar
+  listar(): Observable<Pensamento[]> {
+    return this.http.get<Pensamento[]>(this.API)
+  }
 
 }
